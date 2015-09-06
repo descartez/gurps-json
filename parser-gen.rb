e412342ccf1c68581e36e26435334f8ff7bbb9b0
descartez <rvanstong@gmail.com>
2015-09-06 12:07:04 -0700

    still working on standardization, downcasing all names

--- parser-gen.rb
+++ parser-gen.rb
@@ -6,13 +6,13 @@
 
 # Problems:
 # - json as is is not valid
-# - file needs standardization (downcase all keys and values)?
+# - file needs standardization (downcase all keys and values)? DONE
 # - Turns out some also are attributes. What should we do with these?
+# - some objects only have one modifier, which means it is not in an array. Needs to be in array.
 
 # Should it just save it all into one big file?
 
 require 'fileutils'
-require 'jsonlint'
 require 'json'
 require 'crack'
 
@@ -28,49 +28,49 @@ basic_adv_hash = Crack::XML.parse(basic_set_xml)
 
 def hash_parser(hash, target_hash)
   hash["advantage_list"]["advantage"].each do |adv|
-    p adv["name"].downcase!
+    adv["name"].downcase!
     adv["type"].downcase!
-    if adv["modifiers"].is_a?(Array)
-      if adv["modifiers"].empty? == true
-        adv["modifiers"].each do |mod|
+    if adv["modifier"].is_a?(Array)
+      if adv["modifier"].empty? == false
+        adv["modifier"].each do |mod|
           mod["name"].downcase!
         end
       end
+    elsif !adv["modifier"].is_a?(Array) && !adv["modifier"].nil?
+      adv["modifier"]["name"].downcase!
     end
     if adv["categories"]["category"].is_a?(String)
-      if adv["categories"]["category"].downcase! == "disadvantage"
-        p "disadvantage"
-        target_hash["disadvantages"] << adv
-      elsif adv["categories"]["category"].downcase! == "advantage"
-        p "advantage"
-        target_hash["advantages"] << adv
-      end
+      advantage_string?(adv, target_hash)
     elsif adv["categories"]["category"].is_a?(Array)
-      if adv["categories"]["category"].include?("Disadvantage")
-        p "disadvantage"
-        adv["categories"]["category"].each { |e| p e.downcase! }
-        target_hash["disadvantages"] << adv
-      elsif adv["categories"]["category"].include?("Advantage")
-        p "advantage"
-        adv["categories"]["category"].each { |e| p e.downcase! }
-        target_hash["advantages"] << adv
-      end
+      advantage_array?(adv, target_hash)
     end
   end
+  # p target_hash
 end
 
 def advantage_string?(obj, target_hash)
-
+  if obj["categories"]["category"].downcase! == "disadvantage"
+    target_hash["disadvantages"] << obj
+  elsif obj["categories"]["category"].downcase! == "advantage"
+    target_hash["advantages"] << obj
+  end
 end
 
 def advantage_array?(obj, target_hash)
-
+  if obj["categories"]["category"].include?("Disadvantage")
+    obj["categories"]["category"].each { |e| e.downcase! }
+    target_hash["disadvantages"] << obj
+  elsif obj["categories"]["category"].include?("Advantage")
+    obj["categories"]["category"].each { |e| e.downcase! }
+    target_hash["advantages"] << obj
+  end
 end
 
 basic_set = {"advantages"=>[], "disadvantages"=>[]};
 
-puts "-"*45
-puts "Advantages/Disadvantages"
-puts "-"*45
-
+# puts "-"*45
+# puts "Advantages/Disadvantages"
+# puts "-"*45
+p "running!"
 hash_parser(basic_adv_hash, basic_set)
+p "done!"
